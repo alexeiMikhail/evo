9370cddda198d49871c62af27559444b064c16d0
alexeiMikhail <leleuxc@gmail.com>
2024-12-20 01:33:26 -0800

    Add level 1 scene placeholder and progress bar for alien state change timer

--- alien.gd
+++ alien.gd
@@ -1,9 +1,21 @@
+class_name Alien
 extends CharacterBody2D
 
+@onready var change_timer: Timer = %ChangeTimer
+@onready var progress_bar: ProgressBar = %ProgressBar
 
-const SPEED = 300.0
+
+
+const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
+func _ready() -> void:
+	progress_bar.max_value = change_timer.wait_time
+	print(progress_bar.max_value)
+
+func _process(delta: float) -> void:
+	progress_bar.value = change_timer.time_left
+	print(progress_bar.value, progress_bar.max_value)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +23,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
