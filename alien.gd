9899a7cd7e2e5d48cccc51e32d37eec3d8702192
alexeiMikhail <leleuxc@gmail.com>
2024-12-20 20:50:14 -0800

    Implement alien dash movement

--- alien.gd
+++ alien.gd
@@ -5,12 +5,17 @@ extends CharacterBody2D
 @onready var progress_bar: ProgressBar = %ProgressBar
 @onready var sprite_2d: Sprite2D = %Sprite2D
 @onready var label: Label = %Label
+@onready var dash_timer: Timer = %DashTimer
 
 enum States {RUN, FLY, SWIM, COUNT}
 @export var state: States
 
 var in_water: bool = false
+var is_dashing: bool = false
+var dash_direction: int = 1
 
+
+const DASH_SPEED = 800.0
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
@@ -24,12 +29,19 @@ func _process(_delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	if state == States.RUN:
-		run(delta)
-	if state == States.SWIM:
+	if Input.is_action_just_pressed("dash"):
+		initiate_dash()
+	if is_dashing:
+		velocity.x = DASH_SPEED * dash_direction
+		velocity.y = 0
+		move_and_slide()
+		return
+	elif state == States.SWIM:
 		swim(delta)
-	if state == States.FLY:
+	elif state == States.FLY:
 		fly(delta)
+	elif state == States.RUN:
+		run(delta)
 
 func change_state():
 	state = (state + 1) % States.COUNT
@@ -95,3 +107,15 @@ func fly(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func initiate_dash():
+	is_dashing = true
+	dash_timer.start()
+	if sprite_2d.flip_h:
+		dash_direction = -1
+	else:
+		dash_direction = 1
+		
+
+func _on_dash_timer_timeout() -> void:
+	is_dashing = false
