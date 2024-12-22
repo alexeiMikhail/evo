f0b1274dff48de9ce39145ed5161a6f7726de1d4
Asimile <AsimNRaja@gmail.com>
2024-12-21 22:06:05 -0500

    Wrecking ball swinging and hitbox functionality
    Ball constantly swings back and forth, and if it touches the player they die.
    
    Uses an infinitely looped tween with two tween properties.

--- wrecking_ball.gd
+++ wrecking_ball.gd
@@ -1,15 +1,33 @@
 extends Node2D
 
-# Variables to control the swing
-var swing_speed: float = 2.0  # Speed of the swing
-var swing_amplitude = 90  # Maximum angle (in radians)
+@onready var line: Line2D = %wrecking_line
+@onready var ball = %placeholderBall
 
+# tween variables
 var tween = create_tween()
+var swing_speed: float = 1.7  # Speed of the swing
+var swing_degree: int = 90  # Maximum angle (in radians)
 
 func _ready() -> void:
-	pass
+	# Sets tween's eases, the rates it speeds and slows its process
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_CUBIC)
+	# Queues up two tween animations into our one tween object, then sets them to be looped forever
+	tween.tween_property(self, "rotation_degrees", -swing_degree, swing_speed)
+	tween.tween_property(self, "rotation_degrees", swing_degree, swing_speed)
+	tween.set_loops()
+	# Draws initial line between wrecking ball base and the ball itself
+	line.add_point(Vector2.ZERO, 0)
+	line.add_point(Vector2(ball.position), 1)
 
 
-#func swing():
-	#tween.tween_property(self, "rotation_degrees", swing_amplitude, 1)
-	#swing_amplitude = -swing_amplitude
+func _process(float) -> void:
+	# Updates line to stay connected to ball
+	line.set_point_position(1, Vector2(ball.position))
+
+
+func _on_area_2d_body_entered(body):
+	# Kills player if they touch the wrecking ball
+	if body is Alien:
+		var alien: Alien = body
+		alien.die()
