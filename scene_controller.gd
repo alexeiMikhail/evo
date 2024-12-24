cd4169bdc7705fe9ce0b5cd0719cff65384ff0af
Asimile <AsimNRaja@gmail.com>
2024-12-24 00:31:33 -0500

    Fixed respawn bug spawning where you died

--- scene_controller.gd
+++ scene_controller.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var animation_player = $AnimationPlayer
+@onready var current_stage_load = $level_1
 
 var advancing = false
 
@@ -18,6 +19,7 @@ func _on_animation_player_animation_finished(anim_name):
 		var RELOAD = load("res://level_1.tscn").instantiate()
 		RELOAD.visible = false
 		add_child(RELOAD)
-		$Level1.queue_free()
-		RELOAD.visible = true
+		current_stage_load.queue_free()
+		current_stage_load = RELOAD
+		current_stage_load.visible = true
 		animation_player.play("fade_out")
